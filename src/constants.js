11992c61d0c82816a2f3742e00c57f94e4a4587f
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-02 09:25:03 +0530

    constants and cl styles added

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+const categories = [
+  "All",
+  "Music Videos",
+  "Lyric Videos",
+  "Bloopers",
+  "Live Performances",
+];
+
+const actionType = {
+  DATA: {
+    SET_VIDEOS: "SET_VIDEOS",
+  },
+};
+
+const routes = {
+  VIDEO_LISTING_PAGE: "",
+};
+
+const testCredentials = {
+  email: "test@gmail.com",
+  password: "test123",
+};
+
+export { categories, actionType, routes, testCredentials };
