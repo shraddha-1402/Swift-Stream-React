e4eb43860c9b1a3fb614b7ba46a9cea3ddad59a3
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-02 08:57:10 +0530

    feat: video listing page added

---
+++ src/pages/VideoListingPage.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { useData } from "../context";
+import { CategoryChip, VideoCard } from "../components/";
+import { getCategoryFilteredVideos } from "../utils";
+
+const VideoListingPage = () => {
+  const {
+    dataState: { videos },
+  } = useData();
+  const [filterCategory, setFilterCategory] = useState({ type: "All" });
+
+  const filteredVideos = getCategoryFilteredVideos(filterCategory.type, videos);
+
+  return (
+    <div>
+      <CategoryChip
+        filterCategory={filterCategory}
+        setFilterCategory={setFilterCategory}
+      />
+      <h3 className="mb-1">Most Viewed Videos</h3>
+      <div className="grid-layout">
+        {filteredVideos.map((video) => (
+          <VideoCard key={video._id} video={video} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export { VideoListingPage };
