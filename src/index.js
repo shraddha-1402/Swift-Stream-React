5767324c285d765a6bca713856b521f36a2e7428
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-02 09:03:24 +0530

    updated files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { DataProvider } from "./context";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
