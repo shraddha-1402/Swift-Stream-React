f995de34cd79e84cfb361d9b16e765e411e5d586
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-02 17:56:32 +0530

    feat: login, signup profile routes and custom links added

--- src/components/Menubar/index.jsx
+++ src/components/Menubar/index.jsx
@@ -1,11 +1,15 @@
 import "./style.css";
 import React from "react";
 import PropTypes from "prop-types";
-import { AiOutlineHome,  } from "react-icons/ai";
+import { AiOutlineVideoCamera, AiOutlineUser  } from "react-icons/ai";
 import { CustomNavLink } from "../index";
+import { useAuth } from "../../context";
 import { routes } from "../../constants";
 
 const Menubar = ({ setMenubarActive, menubarActive }) => {
+  const {
+    authState: { token },
+  } = useAuth();
 
   const clickHandler = () => {
     if (menubarActive) setMenubarActive((prev) => !prev);
@@ -26,7 +30,21 @@ const Menubar = ({ setMenubarActive, menubarActive }) => {
           clickHandler={clickHandler}
         >
           <span className="flex-row align-center">
-            <AiOutlineHome className="xs-icon mr-0-25" /> Home
+            <AiOutlineVideoCamera className="xs-icon mr-0-25" /> Explore
+          </span>
+        </CustomNavLink>
+      </li>
+      <li>
+        <CustomNavLink
+          activeClassName="gray-bg text-bold-weight"
+          className="link menubar-items"
+          inactiveClassName="gray-text"
+          to={token ? routes.PROFILE_PAGE : routes.LOGIN_PAGE}
+          clickHandler={clickHandler}
+        >
+          <span className="flex-row align-center">
+            <AiOutlineUser className="xs-icon mr-0-25" />
+            {token ? "Profile" : "Login"}
           </span>
         </CustomNavLink>
       </li>
