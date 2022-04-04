edb31874cd1e0ae830b87417e31eec0f32cfbb0d
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-04 20:09:11 +0530

    fix: removed all video fetch from useEffect

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,10 +1,9 @@
-import axios from "axios";
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { VideoCard } from "../components";
-import { actionType, routes } from "../constants";
+import { routes } from "../constants";
 import { useAuth, useData } from "../context";
-import { clearHistory } from "../utils/services";
+import { clearHistory, getAllHistoryVideos } from "../utils/services";
 
 const HistoryPage = () => {
   const {
@@ -18,25 +17,7 @@ const HistoryPage = () => {
   } = useAuth();
 
   useEffect(() => {
-    (async () => {
-      try {
-        const {
-          data,
-          status,
-          statusText,
-        } = await axios.get("/api/user/history", {
-          headers: { authorization: token },
-        });
-        if (status === 200)
-          dataDispatch({
-            type: actionType.DATA.ADD_TO_HISTORY,
-            payload: data.history,
-          });
-        else throw new Error(statusText);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
+    getAllHistoryVideos({token, dataDispatch});
   }, []);
 
   return (
@@ -47,7 +28,7 @@ const HistoryPage = () => {
             <h3>Recenty Viewed</h3>
             <button
               className="btn btn-solid-danger sm-btn"
-              onClick={() => clearHistory(token, dataDispatch)}
+              onClick={() => clearHistory({token, dataDispatch})}
             >
               Clear
             </button>
