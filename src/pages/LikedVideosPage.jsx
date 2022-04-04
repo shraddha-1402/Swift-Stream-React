6a75567932cbc1525272664d6001449556d58b53
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-04 10:02:26 +0530

    fix: refactored get liked videos, added liked videos count

--- src/pages/LikedVideosPage.jsx
+++ src/pages/LikedVideosPage.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { VideoCard } from "../components";
 import { useAuth, useData } from "../context";
-import { actionType, routes } from "../constants";
-import axios from "axios";
+import { routes } from "../constants";
+import { getLikedVideosHandler } from "../utils/services";
 
 const LikedVideosPage = () => {
   const {
@@ -16,31 +16,14 @@ const LikedVideosPage = () => {
   } = useAuth();
 
   useEffect(() => {
-    (async () => {
-      try {
-        const { data, status, statusText } = await axios.get(
-          "/api/user/likes",
-          { headers: { authorization: token } }
-        );
-        if (status === 200)
-          dataDispatch({
-            type: actionType.DATA.UPDATE_LIKES,
-            payload: {
-              likes: data.likes,
-            },
-          });
-        else throw new Error(statusText);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
+    getLikedVideosHandler(token, dataDispatch);
   }, []);
 
   return (
     <div>
       {likes.length ? (
         <>
-          <h3 className="mb-1">Liked Videos</h3>
+          <h3 className="mb-1">Liked Videos ( {likes.length} )</h3>
           <div className="grid-layout">
             {likes.map((video) => (
               <VideoCard key={video._id} video={video} />
