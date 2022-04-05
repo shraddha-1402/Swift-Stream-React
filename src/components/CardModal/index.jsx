ce94c5ea323a09288cea7b826998d0a9aa36bb40
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-05 08:45:26 +0530

    feat: playlist click handler added

--- src/components/CardModal/index.jsx
+++ src/components/CardModal/index.jsx
@@ -3,33 +3,50 @@ import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import { useOnClickOutside } from "../../hooks";
 import { deleteFromHistoryHandler } from "../../utils/services";
-import { useAuth, useData } from "../../context";
+import { useAuth, useData, usePlaylist } from "../../context";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../../constants";
 
 const CardModal = ({ video, setModalOpen, rest }) => {
-  const { isInHistory } =
-    Object.entries(rest).length > 0 ? rest : false;
-    
+  const { isInHistory } = Object.entries(rest).length > 0 ? rest : false;
+
   const ref = useRef();
   useOnClickOutside(ref, () => setModalOpen(false));
+  const navigate = useNavigate();
 
   const { dataDispatch } = useData();
   const {
     authState: { token },
   } = useAuth();
 
+  const { setSelectedVideo, setShowPlaylistModal } = usePlaylist();
+
+  const handlePlaylistClick = () => {
+    if (!token) navigate(routes.LOGIN_PAGE);
+    else {
+      setModalOpen(false);
+      setSelectedVideo(video);
+      setShowPlaylistModal(true);
+    }
+  };
+
   return (
     <ul ref={ref} className="list-style-none pos-abs card-modal">
       <li className="card-modal-item" onClick={() => setModalOpen(false)}>
         Add to Watch Later
       </li>
-      <li className="card-modal-item" onClick={() => setModalOpen(false)}>
+      <li className="card-modal-item" onClick={handlePlaylistClick}>
         Save to Playlist
       </li>
       {isInHistory && (
         <li
           className="card-modal-item red-text"
           onClick={() => {
-            deleteFromHistoryHandler({token, videoId:video._id, dataDispatch});
+            deleteFromHistoryHandler({
+              token,
+              videoId: video._id,
+              dataDispatch,
+            });
             setModalOpen(false);
           }}
         >
